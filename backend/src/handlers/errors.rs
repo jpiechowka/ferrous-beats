c97a2f4347735ebd8ebed23ac68d90a7fa70fc6c
Janusz Piechówka <januszpiechowka@gmail.com>
2024-08-15 03:00:11 +0200

    Add error chain when returning error responses

--- backend/src/handlers/errors.rs
+++ backend/src/handlers/errors.rs
@@ -13,9 +13,19 @@ where
 }
 impl IntoResponse for ServerError {
     fn into_response(self) -> axum::response::Response {
+        let error_chain = self
+            .0
+            .chain()
+            .map(|e| format!("    {}", e))
+            .collect::<Vec<_>>()
+            .join("\n");
+
         (
             StatusCode::INTERNAL_SERVER_ERROR,
-            format!("Something went wrong: {}", self.0),
+            format!(
+                "Something went wrong: {}\n\nError chain:\n{}\n",
+                self.0, error_chain
+            ),
         )
             .into_response()
     }
