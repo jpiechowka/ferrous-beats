5e18e4718432aa2f6e67198cdb258e9840d00ee5
Janusz Piechówka <januszpiechowka@gmail.com>
2024-08-15 21:56:07 +0200

    Add checking of yt-dlp status

--- backend/src/handlers/yt_dlp/status.rs
+++ backend/src/handlers/yt_dlp/status.rs
@@ -5,12 +5,26 @@ use axum::http::StatusCode;
 use axum::Json;
 use serde::Serialize;
 use std::path::Path;
-use tracing::{debug, instrument, warn};
+use tokio::fs::metadata;
+use tokio::process::Command;
+use tracing::{debug, error, info, instrument};
 
 #[derive(Debug, Serialize)]
 pub struct YtDlpStatusResponse {
-    exists: bool,
+    executable_exists: bool,
     path: String,
+    executable_version: Option<String>,
+    version_execution_results: VersionExecutionResults,
+}
+
+#[derive(Debug, Serialize)]
+pub struct VersionExecutionResults {
+    was_version_command_run: bool,
+    command_completed_successfully: bool,
+    command_error: Option<String>,
+    exit_status: Option<i32>,
+    stdout: Option<String>,
+    stderr: Option<String>,
 }
 
 #[instrument(err, skip(app_state))]
@@ -19,17 +33,103 @@ pub async fn handle_yt_dlp_status(
 ) -> Result<(StatusCode, Json<YtDlpStatusResponse>), ServerError> {
     debug!("Handling checking of yt-dlp status");
 
-    let dlp_dir = &app_state.config.server_settings.dlp_download_dir;
-    let exists = Path::new(dlp_dir).is_dir();
+    let os = std::env::consts::OS;
+    let dlp_dir = Path::new(&app_state.config.server_settings.dlp_download_dir);
+    let executable_path = dlp_dir.join(if os == "windows" {
+        "yt-dlp.exe"
+    } else {
+        "yt-dlp"
+    });
 
-    let status = YtDlpStatusResponse {
-        exists,
-        path: dlp_dir.to_string(),
-    };
-
-    if !exists {
-        warn!("yt-dlp download directory does not exist: {}", dlp_dir);
+    let executable_exists = metadata(&executable_path).await.is_ok();
+    if !executable_exists {
+        return Ok((
+            StatusCode::BAD_REQUEST,
+            Json(YtDlpStatusResponse {
+                executable_exists: false,
+                executable_version: None,
+                path: executable_path.to_string_lossy().to_string(),
+                version_execution_results: VersionExecutionResults {
+                    was_version_command_run: false,
+                    command_completed_successfully: false,
+                    command_error: None,
+                    exit_status: None,
+                    stdout: None,
+                    stderr: None,
+                },
+            }),
+        ));
     }
 
-    Ok((StatusCode::OK, Json(status)))
+    info!("Running yt-dlp version command");
+
+    match Command::new(&executable_path)
+        .arg("--versionz")
+        .output()
+        .await
+    {
+        Ok(output) => {
+            let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+            let stderr = String::from_utf8_lossy(&output.stderr).to_string();
+            let exit_status = output.status.code();
+            let command_completed_successfully = exit_status.map_or(false, |code| code == 0);
+
+            info!("yt-dlp version command was executed");
+
+            Ok((
+                if command_completed_successfully {
+                    StatusCode::OK
+                } else {
+                    StatusCode::BAD_REQUEST
+                },
+                Json(YtDlpStatusResponse {
+                    executable_exists: true,
+                    executable_version: if command_completed_successfully {
+                        Some(parse_version(&stdout))
+                    } else {
+                        None
+                    },
+                    path: executable_path.to_string_lossy().to_string(),
+                    version_execution_results: VersionExecutionResults {
+                        was_version_command_run: true,
+                        command_completed_successfully,
+                        command_error: None,
+                        exit_status,
+                        stdout: Some(stdout),
+                        stderr: Some(stderr),
+                    },
+                }),
+            ))
+        }
+        Err(err) => {
+            error!("Error running yt-dlp version command: {}", err);
+
+            Ok((
+                StatusCode::BAD_REQUEST,
+                Json(YtDlpStatusResponse {
+                    executable_exists: true,
+                    executable_version: None,
+                    path: executable_path.to_string_lossy().to_string(),
+                    version_execution_results: VersionExecutionResults {
+                        was_version_command_run: true,
+                        command_completed_successfully: false,
+                        command_error: Some(err.to_string()),
+                        exit_status: None,
+                        stdout: None,
+                        stderr: None,
+                    },
+                }),
+            ))
+        }
+    }
+}
+
+#[instrument]
+fn parse_version(version_string: &str) -> String {
+    debug!("Parsing yt-dlp version string: {}", version_string);
+    version_string
+        .trim()
+        .chars()
+        .filter(|c| !c.is_control())
+        .collect()
 }
--- backend/src/handlers/yt_dlp/update.rs
+++ backend/src/handlers/yt_dlp/update.rs
@@ -1,9 +1,34 @@
+use crate::handlers::errors::ServerError;
+use crate::AppState;
+use axum::extract::State;
+use axum::http::StatusCode;
+use axum::Json;
 use serde::{Deserialize, Serialize};
+use tracing::{debug, instrument};
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+pub enum YtDlpUpdateChannels {
+    Stable,
+    Master,
+    Nightly,
+}
 
 #[derive(Debug, Deserialize)]
-pub struct YtDlpUpdateRequest {}
+pub struct YtDlpUpdateRequest {
+    update_channel: YtDlpUpdateChannels,
+}
+
 #[derive(Debug, Serialize)]
 pub struct YtDlpUpdateResponse {
-    exists: bool,
-    path: String,
+    stdout: Option<String>,
+    stderr: Option<String>,
+}
+
+#[instrument(err, skip(app_state))]
+pub async fn handle_yt_dlp_update(
+    State(app_state): State<AppState>,
+) -> Result<(StatusCode, Json<YtDlpUpdateResponse>), ServerError> {
+    debug!("Handling updating of yt-dlp");
+    todo!()
 }
