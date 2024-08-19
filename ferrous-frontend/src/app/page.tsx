0be250b62bab5e64558af43e6d3ad125d8673056
Janusz Piechówka <januszpiechowka@gmail.com>
2024-08-19 13:11:03 +0200

    Add table component

--- ferrous-frontend/src/app/page.tsx
+++ ferrous-frontend/src/app/page.tsx
@@ -1,40 +1,14 @@
 import React from "react";
-import {Button, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/react";
+import FerrousNavbar from "@/components/navbar";
+import FerrousTable from "@/components/table";
 
 
 export default function Home() {
     return (
-        <Navbar isBordered>
-            <NavbarBrand>
-                <p className="font-bold text-inherit">ACME</p>
-            </NavbarBrand>
-            <NavbarContent className="hidden sm:flex gap-4" justify="center">
-                <NavbarItem>
-                    <Link color="foreground" href="#">
-                        Features
-                    </Link>
-                </NavbarItem>
-                <NavbarItem isActive>
-                    <Link href="#" aria-current="page">
-                        Customers
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link color="foreground" href="#">
-                        Integrations
-                    </Link>
-                </NavbarItem>
-            </NavbarContent>
-            <NavbarContent justify="end">
-                <NavbarItem className="hidden lg:flex">
-                    <Link href="#">Login</Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Button as={Link} color="primary" href="#" variant="flat">
-                        Sign Up
-                    </Button>
-                </NavbarItem>
-            </NavbarContent>
-        </Navbar>
+        <>
+            <FerrousNavbar></FerrousNavbar>
+
+            <FerrousTable></FerrousTable>
+        </>
     );
 }
